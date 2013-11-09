d7d5a3664d739691898425c811bdc68f3b4133da
Michael Lascarides <mlascarides@gmail.com>
2013-11-09 14:24:32 +1300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PPMigration::Application.config.secret_key_base = '319a1788df60c9fedcb2a68ba4f706337ed39ee7fc3046380d02e46fd7f9ad16917e8f0d4294d1f38a4e3524d0c9c88b6c58abade3bf273e5a2ef9701174d7cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PPMigration::Application.config.session_store :cookie_store, key: '_PP-Migration_session'
