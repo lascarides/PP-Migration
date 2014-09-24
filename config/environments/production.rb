62eea26888caae51e9ebf571cce5948fa468bdba
Michael Lascarides <mlascarides@gmail.com>
2014-09-24 17:14:14 +1200

    vendor compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,9 +31,10 @@ PPMigration::Application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+  config.assets.initialize_on_precompile = false
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
