b708b77eba2477ff822db159c0f5e8500a7f0bfa
Michael Lascarides <mlascarides@gmail.com>
2014-09-28 20:56:44 +1300

    Getting a better handle on facet design

--- app/controllers/newspapers_controller.rb
+++ app/controllers/newspapers_controller.rb
@@ -1,19 +1,21 @@
 class NewspapersController < FormatViewerController
 
 	def title
-		@item 	= NewspaperTitle.find(params[:id])
-		@code 	= NewspaperTitle.code(params[:id])
-		@year 	= @item[:date_year] = params[:year].to_i
-		@month 	= @item[:date_month] = params[:month].to_i
-		@day 	= @item[:date_day] = params[:day].to_i
-		if @day > 0
-			@today = @item[:date] = Date.new(@year, @month, @day)
-			@num_pages = Newspaper.num_pages_for_date(@today, @code)
-			if not params[:page].nil?
-				@page = @item[:page_number]= params[:page].to_i
-				render :action => 'page'
-			else
-				render :action => 'issue'
+		if not params[:id].nil?
+			@item 	= NewspaperTitle.find(params[:id])
+			@code 	= NewspaperTitle.code(params[:id])
+			@year 	= @item[:date_year] = params[:year].to_i
+			@month 	= @item[:date_month] = params[:month].to_i
+			@day 	= @item[:date_day] = params[:day].to_i
+			if @day > 0
+				@today = @item[:date] = Date.new(@year, @month, @day)
+				@num_pages = Newspaper.num_pages_for_date(@today, @code)
+				if not params[:page].nil?
+					@page = @item[:page_number]= params[:page].to_i
+					render :action => 'page'
+				else
+					render :action => 'issue'
+				end
 			end
 		end
 	end
